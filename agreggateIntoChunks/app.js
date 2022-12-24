5562ed086382becb5e32b80bfd0c1abecb40b72a
Jakub <97707577+agenttomek@users.noreply.github.com>
2022-12-24 05:31:39 +0100

    Add files via upload

--- agreggateIntoChunks/app.js
+++ agreggateIntoChunks/app.js
@@ -1,32 +1,33 @@
 var alphabet = "abcdefghijklmnoprstuwxyz".split("");
 var countElements = 0;
-var getRandomNumber = function (array) {
+// coaching habbit
+var getRandomNumber = function () {
     var biggestLengthOfChunk = 8;
     var samllestLengthOfChunk = 4;
-    if (countElements >= array.length - samllestLengthOfChunk - 6)
-        biggestLengthOfChunk = 7;
-    if (countElements >= array.length - samllestLengthOfChunk - 5)
-        biggestLengthOfChunk = 6;
-    if (countElements >= array.length - samllestLengthOfChunk - 4)
-        biggestLengthOfChunk = 5;
-    return Math.floor(Math.random() * (biggestLengthOfChunk - 4)) + 4;
+    return (Math.floor(Math.random() * (biggestLengthOfChunk - samllestLengthOfChunk)) +
+        samllestLengthOfChunk);
 };
 var aggregateIntoChunks = function (array) {
     if (array.length < 1)
         return;
     var result = [];
     var chunk = [];
-    var randomNumber = getRandomNumber(array);
-    for (var i = 0; i < array.length; i++) {
+    var randomNumber = getRandomNumber();
+    for (var i = 0; countElements <= array.length; i++) {
         chunk.push(array[i]);
         if (chunk.length === randomNumber) {
             countElements += randomNumber;
             result.push(chunk);
-            randomNumber = getRandomNumber(array);
+            randomNumber = getRandomNumber();
             chunk = [];
         }
-        if (countElements >= array.length - 7)
-            randomNumber = array.length - countElements;
+        if (countElements > array.length) {
+            result = result.slice(0, -2);
+            countElements = result.flat().length;
+            i = result.flat().length - 1;
+        }
+        if (countElements === array.length)
+            return result;
     }
     return result;
 };
--- agreggateIntoChunks/app.ts
+++ agreggateIntoChunks/app.ts
@@ -2,38 +2,43 @@ const alphabet: string[] = "abcdefghijklmnoprstuwxyz".split("");
 
 let countElements: number = 0;
 
-const getRandomNumber = function (array) {
+// coaching habbit
+
+const getRandomNumber = function () {
   let biggestLengthOfChunk: number = 8;
 
   let samllestLengthOfChunk = 4;
-  if (countElements >= array.length - samllestLengthOfChunk - 6)
-    biggestLengthOfChunk = 7;
-  if (countElements >= array.length - samllestLengthOfChunk - 5)
-    biggestLengthOfChunk = 6;
-  if (countElements >= array.length - samllestLengthOfChunk - 4)
-    biggestLengthOfChunk = 5;
-  return Math.floor(Math.random() * (biggestLengthOfChunk - 4)) + 4;
+  return (
+    Math.floor(Math.random() * (biggestLengthOfChunk - samllestLengthOfChunk)) +
+    samllestLengthOfChunk
+  );
 };
 
 const aggregateIntoChunks = function <T>(array: T[]) {
   if (array.length < 1) return;
 
-  const result: T[][] = [];
+  let result: T[][] = [];
 
   let chunk: T[] = [];
 
-  let randomNumber: number = getRandomNumber(array);
+  let randomNumber: number = getRandomNumber();
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; countElements <= array.length; i++) {
     chunk.push(array[i]);
     if (chunk.length === randomNumber) {
       countElements += randomNumber;
       result.push(chunk);
-      randomNumber = getRandomNumber(array);
+      randomNumber = getRandomNumber();
       chunk = [];
     }
-    if (countElements >= array.length - 7)
-      randomNumber = array.length - countElements;
+
+    if (countElements > array.length) {
+      result = result.slice(0, -2);
+      countElements = result.flat().length;
+      i = result.flat().length - 1;
+    }
+
+    if (countElements === array.length) return result;
   }
 
   return result;
